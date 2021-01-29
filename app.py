24cf561c20d784ac9b150a226779c55ebf1abb5a
Sebastian Hernandez <shernandez@agricapital.com.co>
2021-01-28 22:08:29 -0500

    unidecode

--- app.py
+++ app.py
@@ -47,8 +47,9 @@ def limpiezatexto(text):
   text = text.replace('/',' ')
   #text = text.translate(str.maketrans('', '', string.punctuation))
   text = text.lower()
-  text = ' '.join([word for word in text.split() if word not in stop_words])
   text = unidecode.unidecode(text)
+  text = ' '.join([word for word in text.split() if word not in stop_words])
+  
   return text
 
 import re
